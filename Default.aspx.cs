f955316c2a2749d3f96b820c10eacd2b666ad8ed
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-03-14 23:22:16 +0545

    Committed the Default.aspx.cs code-behind file that handles the server-side logic for the default page.

---
+++ Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+
+namespace KumariCinema
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["CurrentUser"] != null)
+            {
+                Response.Redirect("~/pages/dashboard.aspx", true);
+                return;
+            }
+
+            Response.Redirect("~/components/Login.aspx", true);
+        }
+    }
+}
