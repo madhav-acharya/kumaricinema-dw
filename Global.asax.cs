6ac15ba86b31b2fc6b17de44821075e7f24e159b
Madhav Acharya <np05cp4s240068@iic.edu.np>
2026-03-16 22:41:52 +0545

    Modified Global.asax.cs to reflect recent changes in application lifecycle management and event handling logic.

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,7 @@ namespace KumariCinema
     {
         void Application_Start(object sender, EventArgs e)
         {
+            OracleConfiguration.BindByName = true;
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
